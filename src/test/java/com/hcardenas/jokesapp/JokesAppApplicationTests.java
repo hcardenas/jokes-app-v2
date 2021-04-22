6de8f3d27b5f1e46eb9a6f29027ba4bd964c9e6f
helmut cardenas <aleksey89col@gmail.com>
2021-04-22 14:28:40 -0500

    init

---
+++ src/test/java/com/hcardenas/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcardenas.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
